302a3a19ac8c09cbf54306deb99bcfc49cb88fc2
Administrator <Administrator@ibnprod002611>
2020-05-08 01:14:35 +0530

    testmessage

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 	
 	@RequestMapping
 	public String returnHelloWorld() {
-		return "Welcome to Jenkis with Springboot Journey";
+		return "Welcome to Jenkis with Springboot Journey with Git Hub";
 	}
 
 }
